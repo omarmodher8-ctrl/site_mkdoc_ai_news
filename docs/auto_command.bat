705b4a60d86d9117e64297e97aa625cfba217096
komiyamma <komiyama0629@gmail.com>
2026-04-11 23:20:02 +0900

    本日分追加

--- docs/auto_command.bat
+++ docs/auto_command.bat
@@ -1,8 +1,3 @@
-call :main %*
-4>>"%~f0" goto :eof
-
-:main
-
 @echo off
 REM バッチファイルのあるディレクトリに移動
 cd /d %~dp0
@@ -20,7 +15,5 @@ call codex --yolo -m "gpt-5.4-mini" exec "@gemini_command.mdの内容を実行して。画
 REM call gemini --yolo --prompt "@gemini_command.mdの内容を実行して。画面には進捗以外の情報が出す必要はありません。最後にこのリポジトリに対して、適切なコミットメッセージとともに全てコミット＆プッシュしてください。"
 
 REM qwen --yolo exec "@gemini_command.mdの内容を実行して。画面には進捗以外の情報が出す必要はありません。最後にこのリポジトリに対して、適切なコミットメッセージとともに全てコミット＆プッシュしてください。"
-
-:eof
 
 exit
